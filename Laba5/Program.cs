535670011b96e12c8635bf3d43dc784e8dd570f1
Радомир Князев <kniazev.radomyr@gmail.com>
2019-05-29 12:16:21 +0300

    Добавьте файлы проекта.

---
+++ Laba5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Laba5
+{
+    class Program
+    {
+        static Student[] ReadData(string fileName)
+        {
+            // TODO   implement this method.
+            // It should read the file whose fileName has been passed and fill
+            StreamReader reader = new StreamReader("input.txt");
+            //We might use the line below
+            //int count = System.IO.File.ReadAllLines("file.txt").Length
+            List<Student> students = new List<Student>();
+            while (!reader.EndOfStream)
+            {
+                students.Add(new Student(reader.ReadLine()));
+            }
+            reader.Close();
+            return students.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            Student[] studs = ReadData("input.txt");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba5/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Laba5
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+
+        public Student(string lineWithAllData)
+        {
+            string[] entries = Regex.Split(lineWithAllData, @"\s+");
+            surName = entries[0];
+            firstName = entries[1];
+            patronymic = entries[2];
+            sex = Convert.ToChar(entries[3]);
+            dateOfBirth = entries[4];
+            mathematicsMark = Convert.ToChar(entries[5]);
+            physicsMark = Convert.ToChar(entries[6]);
+            informaticsMark = Convert.ToChar(entries[7]);
+            scholarship = Convert.ToInt32(entries[8]);
+            // TODO   you SHOULD IMPLEMENT constructor with exactly this signature
+            // lineWithAllData is string contating all data about one student, as described in statement
+        }
+    }
+}
