c5c947d827ea02629eb7a27944481de8597882db
Радомир Князев <kniazev.radomyr@gmail.com>
2019-05-30 11:10:49 +0300

    FindMiddleMark + FindStudentWithMiddleMarkMoreThanTotal

--- Laba5/Program.cs
+++ Laba5/Program.cs
@@ -28,8 +28,98 @@ namespace Laba5
         static void Main(string[] args)
         {
             Student[] studs = ReadData("input.txt");
-            
+            double middleMark = FindMiddleMark(studs);
+            FindStudentWithMiddleMarkMoreThanTotal(studs, middleMark);
             Console.ReadKey();
+        }
+
+        private static double FindMiddleMark(Student[] studs)
+        {
+            double middleMark = 0;
+            bool isNum = true;
+            for (int i = 0; i < studs.Length; i++)
+            {
+                double math = 0;
+                double physics = 0;
+                double inf = 0;
+                if (Char.IsDigit(studs[i].mathematicsMark) == isNum) {  
+                    math += Char.GetNumericValue(studs[i].mathematicsMark);
+                }
+                else
+                {
+                    math += 0;
+                }
+                if (Char.IsDigit(studs[i].physicsMark) == isNum)
+                { 
+                    physics += Char.GetNumericValue(studs[i].physicsMark);
+                }
+                else
+                {
+                    physics += 0;
+                }
+                if (Char.IsDigit(studs[i].informaticsMark) == isNum)
+                {
+                    inf += Char.GetNumericValue(studs[i].informaticsMark);
+                }
+                else
+                {
+                    inf += 0;
+                }
+                middleMark += (math + physics + inf);
+                if(i + 1 == studs.Length)
+                {
+                    middleMark /= (3 * studs.Length);
+                    Console.WriteLine("Middle mark of all students: {0}", middleMark);
+                }
+            }
+            return middleMark;
+        }
+
+        private static void FindStudentWithMiddleMarkMoreThanTotal(Student[] studs, double middleMark)
+        {
+            double studentMiddleMark = 0;
+            bool isNum = true;
+            double[] studentsMiddleMark = new double[studs.Length];
+            for (int i = 0; i < studs.Length; i++)
+            {
+                double math = 0;
+                double physics = 0;
+                double inf = 0;
+                if (Char.IsDigit(studs[i].mathematicsMark) == isNum)
+                {
+                    math = Char.GetNumericValue(studs[i].mathematicsMark);
+                }
+                else
+                {
+                    math = 0;
+                }
+                if (Char.IsDigit(studs[i].physicsMark) == isNum)
+                {
+                    physics = Char.GetNumericValue(studs[i].physicsMark);
+                }
+                else
+                {
+                    physics = 0;
+                }
+                if (Char.IsDigit(studs[i].informaticsMark) == isNum)
+                {
+                    inf = Char.GetNumericValue(studs[i].informaticsMark);
+                }
+                else
+                {
+                    inf = 0;
+                }
+                studentMiddleMark = (math + physics + inf) / 3;
+                studentsMiddleMark[i] = studentMiddleMark;
+            }
+
+            for (int i = 0; i < studs.Length; i++)
+            {
+                if(studentsMiddleMark[i] > middleMark)
+                {
+                    Console.WriteLine("Surname: " + studs[i].surName + ", middle mark: " + studentsMiddleMark[i]);
+                }
+            }
         }
     }
 }
